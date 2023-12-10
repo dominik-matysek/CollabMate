f0eade7cd856ea4c2b0979eb3cd3024a1311c1f5
dominik-matysek <dominik0matysek@gmail.com>
2023-12-10 16:50:56 +0100

    first commit

---
+++ server/src/constants/constants.js
@@ -0,0 +1,14 @@
+// File for storing constant values
+
+export const ROLES = {
+  ADMIN: "admin",
+  OWNER: "owner",
+  EMPLOYEE: "employee",
+};
+
+export const TASK_STATUSES = {
+  PENDING: "pending",
+  IN_PROGRESS: "in_progress",
+  COMPLETED: "completed",
+  CLOSED: "closed",
+};
